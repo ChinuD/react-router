cd11746eb510c7cf124c976b61942e9923f077a6
ChinuD <dhokeychinmay.com>
2025-02-21 12:10:58 +0530

    second commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect } from 'react'
+
+function Github() {
+
+    const [data,setData] = useState([])
+
+    // useEffect(()=> {
+    //     fetch('https://api.github.com/users/chinud')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
+
+  return (
+    <>
+        <div className='mx-150'>Github: {data.name}</div>
+        <div className='mx-150'>Github Followers: {data.followers}</div>
+        <img src={data.avatar_url} className='rounded-full w-20 mx-180' />
+    </>
+  )
+}
+
+export default Github
+
+export const githubinfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/chinud')
+    return response.json();
+}
